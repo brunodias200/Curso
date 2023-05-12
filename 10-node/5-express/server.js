900d0272fc2ae10ec4635887757c006777221a39
Bruno Dias <brunoadias1996@gmail.com>
2023-05-12 10:28:43 -0300

    criada public

--- 10-node/5-express/server.js
+++ 10-node/5-express/server.js
@@ -4,17 +4,11 @@ const routes = require('./routes');
 const path = require('path');
 
 app.use(express.urlencoded({ extended:true}));
+app.use(express.static(path.resolve(__dirname, 'public')))
+
 app.use(routes);
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
-
-
-// app.get('/teste/:id_usuario?', (req, res)=>{
-//     console.log(req.params);// params pega o parametro, nesse caso o id_usuario
-//     console.log(req.query);// params pega os query string da url ?nome=bruno&...
-//     res.send(`id usuario ${req.params.id_usuario}`);
-
-// })
 
 port = 3000
 app.listen(port, ()=>{
