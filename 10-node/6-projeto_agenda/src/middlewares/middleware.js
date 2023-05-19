130b268a007ffcffb0918ddb29c47d98b5679c6b
Bruno Dias <brunoadias1996@gmail.com>
2023-05-19 17:28:38 -0300

    atualizando git do curso

--- 10-node/6-projeto_agenda/src/middlewares/middleware.js
+++ 10-node/6-projeto_agenda/src/middlewares/middleware.js
@@ -1,28 +1,19 @@
 exports.middlewareGlobal = (req, res, next) => {
-  if (req.body.cliente) {
-
-    console.log();
-
-    console.log(`Vi que você postou ${req.body.cliente}`);
-
-    console.log();
-
-  }
+  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
   next();
 };
 
 exports.outroMiddleware = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'este é o valor da variavel local'
   next();
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-  if(err && err.code==='EBADCSRFTOKEN'){
+  if(err && 'EBADCSRFTOKEN' === err.code) {
     return res.render('404');
   }
-}
+};
 
 exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
-}
+};
