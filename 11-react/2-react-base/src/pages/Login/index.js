307c4e6a600dad429839ec5de6005fdf1b604746
brunodias200 <76215338+brunodias200@users.noreply.github.com>
2023-06-29 11:48:27 -0300

    introduction to styled components

---
+++ 11-react/2-react-base/src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Title } from './styled';
+
+export default function Login() {
+  return (
+    <>
+      <Title isRed={false}>
+        Página de Login
+        <small>teste</small>
+      </Title>
+      <p>teste testes tes testse es</p>
+    </>
+  );
+}
